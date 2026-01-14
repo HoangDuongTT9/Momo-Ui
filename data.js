f1393e5046eb089cd8e3262269550ed1ca165d01
HoangDuongTT9 <33827197+HoangDuongTT9@users.noreply.github.com>
2026-01-14 16:21:21 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,9 @@
+const data = {
+    name: "HOÀNG TH? HUONG NGUYÊN",
+    contract: "20220712-2904683",
+    debt: "60.435.459d"
+};
+
+document.getElementById("customerName").innerText = data.name;
+document.getElementById("contract").innerText = data.contract;
+document.getElementById("totalDebt").innerText = data.debt;
